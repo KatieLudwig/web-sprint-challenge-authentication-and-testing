9aafa6a3c2fdee207c7bca24af52e107b395572e
Katie Ludwig <katierludwig@gmail.com>
2024-07-24 12:45:16 -0700

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,20 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authoriztion;
+
+  if (!token) {
+    return res.status(401).json({ message: 'token required' });
+  }
+
+  jwt.verify(token, process.env.SECRET || 'shh', (err, decodedToken) => {
+    if (err) {
+      return res.status(401).json({ message: 'token invalie' });
+    } else {
+      req.decodedToken = decodedToken;
+      next();
+    }
+  });
   /*
     IMPLEMENT
 
